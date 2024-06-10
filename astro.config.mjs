5bd0e1bad4a491ed610341025d9f3d7f71c3f64f
Mark Lyck <mark.lyck@gmail.com>
2024-06-09 23:44:25 -0400

    :sparkles: Upgrade deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
-  output: "server",
-  adapter: vercel({
-    analytics: true,
-  }),
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+	],
+	output: "server",
+	adapter: vercel({
+		analytics: true,
+	}),
 });
